e0ab8f2a982bf3b76eb9deef68bbec55a422f11c
jhapriyanshu16 <priyanshu2015madhepur@gmail.com>
2024-07-12 11:01:09 +0530

    Fixed home app issue and successful initial setup done

--- recipeproj/urls.py
+++ recipeproj/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from vege.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',receipes,name="receipes"),
 ]

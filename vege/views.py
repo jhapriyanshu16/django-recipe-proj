910e606b64a5f663e611e2da0762d07a4bfb5e5b
jhapriyanshu16 <priyanshu2015madhepur@gmail.com>
2024-07-12 18:09:19 +0530

    Add Bootstrap form for new recipes and display table

--- vege/views.py
+++ vege/views.py
@@ -1,6 +1,27 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import *
 # Create your views here.
 
 def receipes(request):
-    return render(request,"receipes/receipes.html")
+    if request.method == "POST":
+        data  = request.POST
+
+        receipe_description = data.get('receipe_description')
+        receipe_name = data.get('receipe_name')
+        receipe_image = request.FILES.get('receipe_image')
+        print(receipe_name)
+        print(receipe_description)
+        print(receipe_image)
+
+        Receipe.objects.create(
+            receipe_description = receipe_description,
+            receipe_name = receipe_name,
+            receipe_image = receipe_image ,
+        )
+
+        return redirect('/')
+    
+    queryset = Receipe.objects.all()
+    context = {'receipes':queryset}
+
+    return render(request,"receipes/receipes.html",context)
